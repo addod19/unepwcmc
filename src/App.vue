10004bc9e3b2aa02885330857cdc3917bfc39302
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2022-07-30 00:52:51 +0000

    Install axios, and rename components and it's associated imports

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <MovieListing />
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue';
+import MovieListing from './components/MovieListing.vue';
+
+export default {
+  name: 'App',
+  components: {
+    MovieListing
+  }
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/MovieListing.vue
@@ -0,0 +1,20 @@
+<template>
+    <h3>Planet+</h3>
+
+    <span>
+        We transform how data and Knowledge on biodiversity
+        is collected, analysed, accessed, and used
+    </span>
+</template>
+
+<script>
+    export default {
+        name: 'UserListing',
+    }
+</script>
+
+<style>
+    h3 {
+        margin-bottom: 5%;
+    }
+</style>
