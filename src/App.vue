e49747f1982723bc5a44fad35a151f8468383166
Daniel Larbi Addo <addodaniellarbi@gmail.com>
2022-08-01 11:11:02 +0000

    Add existing project to repo

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld.vue';
 import MovieListing from './components/MovieListing.vue';
 
 export default {
@@ -24,7 +23,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
 </style>
--- src/components/MovieListing.vue
+++ src/components/MovieListing.vue
@@ -1,20 +1,62 @@
 <template>
     <h3>Planet+</h3>
 
-    <span>
-        We transform how data and Knowledge on biodiversity
-        is collected, analysed, accessed, and used
+    <span class="header-wrapper">
+        <h4 class="header-wrapper-test">
+            We transform how data and Knowledge on biodiversity
+            is collected, analysed, accessed, and used
+        </h4>
     </span>
+    <section>
+        <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Id</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">City</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="user in users" v-bind:key="user.id"> 
+          <th scope="row">{{user.id}}</th>
+          <td>{{user.name}}</td>
+          <td>{{user.email}}</td>
+          <td>{{user.address.city}}</td>
+        </tr>
+      </tbody>
+    </table> 
+    </section>
 </template>
 
 <script>
+import axios from 'axios';
+
     export default {
-        name: 'UserListing',
+        name: 'MovieListing',
+        data() {
+            return {
+                movie: null,
+            }
+        },
+        created: () => {
+            axios
+            .get('https://api.spacexdata.com/v3/rockets')
+            .then(res => {
+                console.log(res.data.map(ele => console.log(ele.flickr_images)));
+                this.movie = res.data;
+            })
+        }
     }
 </script>
 
 <style>
     h3 {
         margin-bottom: 5%;
+    }
+    span {
+        width: 20em;
+        height: 25em;
+        border: 1px solid red;
     }
 </style>
